4ac7ab6cc1f1145791072f074a7aff6d755af0c8
unknown <dnt1997@gmail.com>
2023-03-04 21:00:01 +0530

    converting mongo to mysql

--- backend/routes/categoriesRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CategoriesController = require('../controllers/CategoriesController');
-
-
-router.post("/get", CategoriesController.getCategories);
-module.exports = router;
--- backend/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/UserController');
-const tokenMiddleware = require('../middlewares/checkToken');
-
-
-router.post("/edit", tokenMiddleware.checkToken,UserController.editProfile);
-module.exports = router;
